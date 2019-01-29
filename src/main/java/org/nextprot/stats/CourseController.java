327df736a530729241b5372a5b0baf4084c4d024
fnikitin <frederic.nikitin@isb-sib.ch>
2019-01-29 18:27:00 +0100

    Controller produce protobug or json

--- src/main/java/org/nextprot/stats/CourseController.java
+++ src/main/java/org/nextprot/stats/CourseController.java
@@ -1,11 +1,13 @@
 package org.nextprot.stats;
 
 import com.googlecode.protobuf.format.JsonFormat;
+import io.swagger.annotations.ApiOperation;
 import org.nextprot.stats.domain.AddressBook.Course;
 import org.nextprot.stats.domain.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,16 +16,10 @@ public class CourseController {
 	@Autowired
 	private CourseRepository courseRepo;
 
-	@RequestMapping("/courses/protobuf/{id}")
+	@RequestMapping(path="/courses/{id}", method = RequestMethod.GET, produces = { "application/json", "application/x-protobuf"})
+	@ApiOperation("Returns the requested course.")
 	public Course protoBufCustomer(@PathVariable Integer id) {
 
 		return courseRepo.getCourse(id);
-	}
-
-	@RequestMapping("/courses/json/{id}")
-	public String jsonCustomer(@PathVariable Integer id) {
-
-		JsonFormat jsonFormat = new JsonFormat();
-		return jsonFormat.printToString(protoBufCustomer(id));
 	}
 }
